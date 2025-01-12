5c38bba95e82d353e3b3dd9c913793915e17e737
Ali Maaz Akhter <150456791+alimaazakhter@users.noreply.github.com>
2025-01-12 14:51:27 +0530

    Add files via upload

---
+++ listpg3.py
@@ -0,0 +1,12 @@
+#write a program to swap first and fourth element
+A = [10, 60, 20, 90]
+A[0], A[2] = A[2], A[0]
+print(A)
+
+#Add an new value on second 
+A.insert(1, 70)
+print(A)
+
+#Delete a value from 3rd postion
+A.pop(3)
+print(A)
